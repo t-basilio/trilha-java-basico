f2ec41838044301963325b84f0e98f89c278de22
Thiago Basilio <basilio.barboza@gmail.com>
2024-06-01 16:32:00 -0300

    meu projeto conta banco

---
+++ src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        //TODO: impotar a classe Scanner
+
+        //TODO: exibir as mensagens para o usuário
+        //TODO: obter pela scanner os valores digitos no terminal
+
+        //TODO: exibir a mensagem conta criada
+    }
+}
